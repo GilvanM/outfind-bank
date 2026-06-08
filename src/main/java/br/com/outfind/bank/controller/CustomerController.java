70326f5056a2d73a03263ca9e91782283bff6b2b
Gilvan Menezes <gilvan.tude@hotmail.com>
2026-06-08 19:55:43 -0300

    feat: integrate customer controller with service layer

--- src/main/java/br/com/outfind/bank/controller/CustomerController.java
+++ src/main/java/br/com/outfind/bank/controller/CustomerController.java
@@ -1,13 +1,20 @@
 package br.com.outfind.bank.controller;
 
+import br.com.outfind.bank.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping("/customers")
     public String listCustomers() {
-        return "Customer endpoint working";
+        return customerService.listCustomers();
     }
 }
--- src/main/java/br/com/outfind/bank/service/CustomerService.java
+++ src/main/java/br/com/outfind/bank/service/CustomerService.java
@@ -1,4 +1,12 @@
 package br.com.outfind.bank.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    public String listCustomers() {
+        return "Customer endpoint working";
+    }
+
 }
